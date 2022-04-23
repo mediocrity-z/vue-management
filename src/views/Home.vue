c6d672454371298271b0a84583132ae46c1855ca
祝子莘 <1157023204@qq.com>
2022-04-23 20:54:53 +0800

    keep-alive缓存组件;实现导航栏固定和左侧菜单栏禁止滚动

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,9 @@
       <el-button type="info" @click="loginout">退出</el-button></el-header
     >
     <!-- 页面主体区域 -->
-    <el-container>
+    <el-container class="main-container">
       <!-- 侧边栏区域 -->
-      <el-aside :width="isCollapse ? '64px':'200px'">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
         <el-menu
@@ -22,31 +22,41 @@
           active-text-color="#409EFF"
         >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id" >
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i :class="icons[item.id]"></i>
               <!-- 文本 -->
-              <span>{{item.authName}}</span>
+              <span>{{ item.authName }}</span>
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavstate('/'+subItem.path)">
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavstate('/' + subItem.path)"
+            >
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
                 <!-- 文本 -->
-                <span>{{subItem.authName}}</span>
+                <span>{{ subItem.authName }}</span>
               </template>
-              </el-menu-item>
+            </el-menu-item>
           </el-submenu>
-         
         </el-menu>
       </el-aside>
       <!-- 右侧内容主体 -->
       <el-main>
-        <router-view></router-view>
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
       </el-main>
     </el-container>
   </el-container>
@@ -56,27 +66,26 @@
 export default {
   name: "Home",
   //页面刚一加载出来就应该显示出来左侧菜单
-  created(){
-this.getMenuList();
-this.activePath=window.sessionStorage.getItem('activePath')
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem("activePath");
   },
-  data(){
+  data() {
     return {
       //左侧菜单数据
-      menulist:[],
-      icons:{
-        '125': 'el-icon-s-check',
-        '103':'el-icon-s-operation',
-        '101':'el-icon-s-goods',
-        '102':'el-icon-s-order',
-        '145':'el-icon-s-marketing'
-
+      menulist: [],
+      icons: {
+        125: "el-icon-s-check",
+        103: "el-icon-s-operation",
+        101: "el-icon-s-goods",
+        102: "el-icon-s-order",
+        145: "el-icon-s-marketing",
       },
       //默认是展开状态
       isCollapse: false,
       //被激活的链接地址
-      activePath:'',
-    }
+      activePath: "",
+    };
   },
   methods: {
     loginout() {
@@ -85,22 +94,21 @@ this.activePath=window.sessionStorage.getItem('activePath')
       this.$router.push("/login");
     },
     //获取所有的菜单,异步操作
-    async getMenuList()  {
+    async getMenuList() {
       //menus是接口文档中的请求路径
-      const {data:res} =await this.$http.get('menus');
-      if(res.meta.status !==200) return this.$message.error(res.meta.msg);
-      this.menulist=res.data;
-    
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
     },
     //点击按钮，切换左侧菜单的折叠和展开
-    toggleCollapse(){
-     this.isCollapse= !this.isCollapse
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
     },
     //保存链接的激活状态到sessionStorage中的acivePath属性中
-    saveNavstate(activePath){
-     window.sessionStorage.setItem('activePath',activePath);
-     this.activePath=activePath;
-    }
+    saveNavstate(activePath) {
+      window.sessionStorage.setItem("activePath", activePath);
+      this.activePath = activePath;
+    },
   },
 };
 </script>
@@ -108,9 +116,19 @@ this.activePath=window.sessionStorage.getItem('activePath')
 <style lang="less" scoped>
 .home-container {
   height: 100%;
+  position: relative;
 }
-.el-menu{
+.el-menu {
   border-right: none;
+}
+.main-container {
+  margin-top: 60px;
+  height: 100%;
+  overflow: hidden;
+  .el-aside {
+    background-color: #333744;
+    height: 100%;
+  }
 }
 //组件名本身当成一个类选择器使用
 .el-header {
@@ -121,15 +139,17 @@ this.activePath=window.sessionStorage.getItem('activePath')
   align-items: center;
   color: #fff;
   font-size: 20px;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 99999;
 }
-.el-aside {
-  background-color: #333744;
-}
+
 .el-main {
   background-color: #eaedf1;
 }
-.toggle-button{
-  background-color: #4A5064;
+.toggle-button {
+  background-color: #4a5064;
   font-size: 10px;
   line-height: 24px;
   color: #fff;
